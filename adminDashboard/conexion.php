958349a0bdd9f27d350bdc55e331fb2c7d67d125
Sayes29 <migsayes@gmail.com>
2026-06-12 07:59:35 +0200

    Archivos innecesarios eliminados

--- adminDashboard/conexion.php
+++ adminDashboard/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$host = "sql209.byetcluster.com"; // El servidor de tu phpMyAdmin en ByetHost
+$host = "sql209.byetcluster.com"; // El servidor de phpMyAdmin en ByetHost
 $db   = "if0_42105450_bandida";  // nombre de base de datos del hosting
 $user = "if0_42105450";          // usuario de la base de datos
 $pass = "h2Kp08FW9qnQec";     // la contraseña real de tu cuenta de hosting
